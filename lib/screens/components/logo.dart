eecd0283ca858a61b365449a6ba4919b25cd645e
Senuri Anudika <69381128+nvsenuri@users.noreply.github.com>
2021-01-16 17:52:20 +0530

    add logo page

---
+++ lib/screens/components/logo.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Logo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200,
+      height: 200,
+      child: Image.asset("images/logo.png"),
+    );
+  }
+}
